837f19678449da494ec4437ecade210b418f5d60
Duc Hai <haipham@minu.ai>
2023-09-18 17:29:27 +0700

    Add jenkin file

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
-docker run --name minu-cms-base-nginx -v /var/jenkins_home/workspace/green-blue/nginx-cms-base/default.conf:/etc/nginx/conf.d/default.conf:ro -d --restart=unless-stopped --network=wion-dev --log-opt max-size=1G --log-opt max-file=2 -p 9102:80 nginx:stable-alpine 
+docker run --name minu-cms-base-nginx -v /home/thien/workspace/minu-base-cms-dev/nginx/default.conf:/etc/nginx/conf.d/default.conf:ro -d --restart=unless-stopped --network=wion-dev --log-opt max-size=1G --log-opt max-file=2 -p 9100:80 nginx:stable-alpine 
 
 docker run --name minu-cms-base-nginx -v /var/jenkins_home/workspace/green-blue/nginx-cms-base/default:/etc/nginx/conf.d/default:ro --restart=unless-stopped --network=wion-dev --log-opt max-size=1G --log-opt max-file=2 -p 9102:80 nginx:stable-alpine 
+
+
+docker run --name minu-server-base-nginx -v /home/thien/workspace/minu-server-base/nginx/default.conf:/etc/nginx/conf.d/default.conf:ro -d --restart=unless-stopped --network=wion-dev --log-opt max-size=1G --log-opt max-file=2 -p 9001:80 -p 9006:81 nginx:stable-alpine 
